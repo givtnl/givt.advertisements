3c5bb190e7532ef763695471584aefe298c5e30c
Bjorn Derudder <bjorn.derudder@gmail.com>
2021-11-17 15:52:21 +0100

    Initial commit

---
+++ src/GivtAdvertisements/GivtAdvertisementsStack.cs
@@ -0,0 +1,13 @@
+using Amazon.CDK;
+using Constructs;
+
+namespace GivtAdvertisements
+{
+    public class GivtAdvertisementsStack : Stack
+    {
+        internal GivtAdvertisementsStack(Construct scope, string id, IStackProps props = null) : base(scope, id, props)
+        {
+            // The code that defines your stack goes here
+        }
+    }
+}
